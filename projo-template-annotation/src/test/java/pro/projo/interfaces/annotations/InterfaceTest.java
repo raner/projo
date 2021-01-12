293328b00e03b0d3b691a898c43c0390bfab0972
Mirko Raner <mirko@raner.ws>
2021-01-11 21:30:22 -0800

    Issue #65: rename pro.projo.interfaces.annotation package in test folder

--- projo-template-annotation/src/test/java/pro/projo/interfaces/annotations/InterfaceTest.java
+++ projo-template-annotation/src/test/java/pro/projo/interfaces/annotations/InterfaceTest.java
@@ -1,5 +1,5 @@
 //                                                                          //
-// Copyright 2018 Mirko Raner                                               //
+// Copyright 2018 - 2021 Mirko Raner                                        //
 //                                                                          //
 // Licensed under the Apache License, Version 2.0 (the "License");          //
 // you may not use this file except in compliance with the License.         //
@@ -13,18 +13,20 @@
 // See the License for the specific language governing permissions and      //
 // limitations under the License.                                           //
 //                                                                          //
-package pro.projo.interfaces.annotation;
+package pro.projo.interfaces.annotations;
 
 import java.util.concurrent.Callable;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 import org.junit.Test;
-import static javax.lang.model.element.Modifier.PUBLIC;
-import static javax.lang.model.element.Modifier.STATIC;
+import pro.projo.interfaces.annotation.Interface;
+import pro.projo.interfaces.annotation.Interfaces;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static pro.projo.interfaces.annotation.Ternary.TRUE;
+import static pro.projo.interfaces.annotation.Visibility.PUBLIC;
 
 public class InterfaceTest
 {
@@ -43,15 +45,16 @@ public class InterfaceTest
         Interface callable = Stream.of(interfaces.value()).filter(equals(Interface::generate, "Callable")).findFirst().get();
         assertEquals(Callable.class, callable.from());
     }
-    
+
     @Test
     public void testInterfaceAnnotationForMath()
     {
         Package testPackage = getClass().getPackage();
         Interfaces interfaces = testPackage.getAnnotation(Interfaces.class);
         Interface math = Stream.of(interfaces.value()).filter(equals(Interface::generate, "Math")).findFirst().get();
-        Object[][] expected = {{Math.class}, {PUBLIC, STATIC}};
-        assertArrayEquals(expected, new Object[] {new Object[] {math.from()}, math.modifiers()});
+        Object[][] expected = {{Math.class}, {TRUE}, {PUBLIC}};
+        Object[][] actual = {{math.from()}, {math.isStatic()}, math.visibility()};
+        assertArrayEquals(expected, actual);
     }
 
     private <I, T> Predicate<I> equals(Function<I, T> method, T value)
